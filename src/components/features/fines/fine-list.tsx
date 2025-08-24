420549c4c1f60c3cc02102c561beb6ba9082b075
samfeldman824 <samuel_feldman@brown.edu>
2025-08-23 23:20:18 -0400

    remove yellow as avatar color

--- src/components/features/fines/fine-list.tsx
+++ src/components/features/fines/fine-list.tsx
@@ -333,7 +333,6 @@ function CompactFineItem({ fine, onClick }: CompactFineItemProps) {
       'bg-purple-500',
       'bg-pink-500',
       'bg-indigo-500',
-      'bg-yellow-500',
       'bg-teal-500',
       'bg-orange-500',
       'bg-cyan-500',
